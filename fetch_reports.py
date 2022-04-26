b40e852512ac1b76a9af7baa6678b8d5ed2812a7
Kai-Po Lin <charleslin@Kai-Pos-MacBook-Pro.local>
2022-04-26 17:04:59 -0500

    [Dev Fix] Refactor fetching financial reports

--- fetch_reports.py
+++ fetch_reports.py
@@ -1,8 +1,5 @@
-# Self-defined files
 import utils
 import config
-import margin_statements
-
 import pandas as pd
 
 
@@ -61,27 +58,3 @@ def organize_reports(stock_symbol: str) -> pd.DataFrame():
         df = pd.concat([df, fetch_statements(soup)], ignore_index=True)
 
     return df
-
-
-if __name__ == '__main__':
-    with pd.ExcelWriter(config.save_path, engine='openpyxl') as writer:
-        for stock_symbol in config.stock_symbols:
-            # Fetch all given stock symbol's reports in HTML format from Yahoo finanace and extract the
-            # indicators we want from them
-            df = organize_reports(stock_symbol)
-
-            # Filter all nan values and replace some statements' titles
-            df = utils.clean_dataframe(df)
-
-            # Add margin statements into the dataframe
-            df = margin_statements.add_margin_statements(df)
-
-            # Drop titles that are only for margin calculation
-            df = margin_statements.drop_margin_statements(df)
-
-            # Store the dateframe in a specific sheet and named it according to its stock symbol
-            df.to_excel(writer, sheet_name=stock_symbol, index=False)
-
-            print(f"Done scraping {stock_symbol}")
-
-    print(f"\nSuccessfully saved financial all report!\n-> Path: {config.save_path}\n")
